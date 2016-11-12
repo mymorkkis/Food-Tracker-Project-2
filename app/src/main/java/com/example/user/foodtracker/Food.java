9b4a8d0ac248a1bdaa109017c7a13ea9b057d825
Max <mveasey87@gmail.com>
2016-11-12 11:49:43 +0000

    Food getter tests passed

--- app/src/main/java/com/example/user/foodtracker/Food.java
+++ app/src/main/java/com/example/user/foodtracker/Food.java
@@ -5,4 +5,23 @@ package com.example.user.foodtracker;
  */
 
 public class Food {
+
+//    private int id?;
+    private String name;
+    private int calories;
+//    private int mealId?;
+
+    public Food(String name, int calories) {
+        this.name = name;
+        this.calories = calories;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getCalories() {
+        return this.calories;
+    }
+
 }
