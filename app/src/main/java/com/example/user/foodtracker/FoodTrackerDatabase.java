31d37b8f5c0be926a7013f22e17a7f704a06da14
Max <mveasey87@gmail.com>
2016-11-13 14:44:02 +0000

    added insertData method for database & casted the EditText fields in InputMealActivity

--- app/src/main/java/com/example/user/foodtracker/FoodTrackerDatabase.java
+++ app/src/main/java/com/example/user/foodtracker/FoodTrackerDatabase.java
@@ -1,5 +1,6 @@
 package com.example.user.foodtracker;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -13,26 +14,40 @@ public class FoodTrackerDatabase extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "food_tracker.db";
     public static final String TABLE_NAME = "meals";
     public static final String COL_1 = "ID";
-    public static final String COL_1 = "NAME";
-    public static final String COL_1 = "TYPE";
-    public static final String COL_1 = "CALORIES";
+    public static final String COL_2 = "NAME";
+    public static final String COL_3 = "TYPE";
+    public static final String COL_4 = "CALORIES";
 
 
 
     public FoodTrackerDatabase(Context context) {
         super(context, DATABASE_NAME, null, 1);
-        SQLiteDatabase sqLiteDatabase = this.getReadableDatabase();
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL("create table " + TABLE_NAME +
+    public void onCreate(SQLiteDatabase myDatabase) {
+        myDatabase.execSQL("create table " + TABLE_NAME +
                 "(ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, TYPE TEXT, CALORIES INTEGER)" );
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
-        onCreate(sqLiteDatabase);
+    public void onUpgrade(SQLiteDatabase myDatabase, int i, int i1) {
+        myDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(myDatabase);
     }
+
+    public boolean insertData(String name, String type, String calories) {
+        SQLiteDatabase myDatabase = this.getReadableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2, name);
+        contentValues.put(COL_3, type);
+        contentValues.put(COL_4, calories);
+        long result = myDatabase.insert(TABLE_NAME, null, contentValues);
+        if (result == -1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
 }
--- app/src/main/java/com/example/user/foodtracker/InputMealActivity.java
+++ app/src/main/java/com/example/user/foodtracker/InputMealActivity.java
@@ -2,6 +2,8 @@ package com.example.user.foodtracker;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by user on 13/11/2016.
@@ -10,11 +12,23 @@ import android.support.v7.app.AppCompatActivity;
 public class InputMealActivity extends AppCompatActivity {
 
     FoodTrackerDatabase foodTrackerDatabase;
+    EditText editName, editType, editCalories;
+    Button btnAddMeal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_meal);
         foodTrackerDatabase = new FoodTrackerDatabase(this);
+
+        //casting
+        editName = (EditText)findViewById(R.id.text_to_save_name);
+        editType = (EditText)findViewById(R.id.text_to_save_type);
+        editCalories = (EditText)findViewById(R.id.text_to_save_calories);
+        btnAddMeal = (Button)findViewById(R.id.button_input_meal);
     }
+
+    public void AddData() {
+    }
+
 }
