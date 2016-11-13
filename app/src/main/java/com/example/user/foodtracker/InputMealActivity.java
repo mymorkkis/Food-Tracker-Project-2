25dc55133acc70712b798f3baeed334f0911d556
Max <mveasey87@gmail.com>
2016-11-13 15:17:17 +0000

    added AddMeal method to ImputMealActivity

--- app/src/main/java/com/example/user/foodtracker/InputMealActivity.java
+++ app/src/main/java/com/example/user/foodtracker/InputMealActivity.java
@@ -2,8 +2,10 @@ package com.example.user.foodtracker;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by user on 13/11/2016.
@@ -11,24 +13,42 @@ import android.widget.EditText;
 
 public class InputMealActivity extends AppCompatActivity {
 
-    FoodTrackerDatabase foodTrackerDatabase;
-    EditText editName, editType, editCalories;
-    Button btnAddMeal;
+    FoodTrackerDatabase mFoodTrackerDatabase;
+    EditText mEditName, mEditType, mEditCalories;
+    Button mBtnAddMeal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_meal);
-        foodTrackerDatabase = new FoodTrackerDatabase(this);
+        mFoodTrackerDatabase = new FoodTrackerDatabase(this);
 
         //casting
-        editName = (EditText)findViewById(R.id.text_to_save_name);
-        editType = (EditText)findViewById(R.id.text_to_save_type);
-        editCalories = (EditText)findViewById(R.id.text_to_save_calories);
-        btnAddMeal = (Button)findViewById(R.id.button_input_meal);
+        mEditName = (EditText)findViewById(R.id.text_to_save_name);
+        mEditType = (EditText)findViewById(R.id.text_to_save_type);
+        mEditCalories = (EditText)findViewById(R.id.text_to_save_calories);
+        mBtnAddMeal = (Button)findViewById(R.id.button_input_meal);
+        AddMeal();
     }
 
-    public void AddData() {
+    public void AddMeal() {
+        mBtnAddMeal.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        boolean isInsterted = mFoodTrackerDatabase.insertData(
+                                mEditName.getText().toString(), mEditType.getText().toString(),
+                                mEditCalories.getText().toString());
+                        if(isInsterted == true) {
+                            Toast.makeText(InputMealActivity.this,
+                                    "Meal Inserted!", Toast.LENGTH_SHORT).show(); }
+                        else {
+                            Toast.makeText(InputMealActivity.this,
+                                    "WARNING Meal Not Inserted", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }
+        );
     }
 
 }
