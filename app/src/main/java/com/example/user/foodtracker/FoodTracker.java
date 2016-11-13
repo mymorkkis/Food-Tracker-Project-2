5c25879659b70f079d14ac00bad958c1f3e26da1
Max <mveasey87@gmail.com>
2016-11-13 11:23:26 +0000

    added input_meal title to the manifest file

---
+++ app/src/main/java/com/example/user/foodtracker/FoodTracker.java
@@ -0,0 +1,11 @@
+package com.example.user.foodtracker;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by user on 13/11/2016.
+ */
+
+public class FoodTracker extends AppCompatActivity {
+
+}
