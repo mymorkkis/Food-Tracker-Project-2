6e7551e0595d7abce15126857cf52444f4f30c98
Max <mveasey87@gmail.com>
2016-11-13 12:21:45 +0000

    added TextView & EditText widgets to input meal activity

--- app/src/main/java/com/example/user/foodtracker/FoodTracker.java
+++ app/src/main/java/com/example/user/foodtracker/FoodTracker.java
@@ -1,6 +1,10 @@
 package com.example.user.foodtracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 /**
  * Created by user on 13/11/2016.
@@ -8,4 +12,20 @@ import android.support.v7.app.AppCompatActivity;
 
 public class FoodTracker extends AppCompatActivity {
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_input_meal_link) {
+            Intent intent = new Intent(FoodTracker.this, InputMealActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
