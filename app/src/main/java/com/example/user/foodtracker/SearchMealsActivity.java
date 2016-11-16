822bdaf2163bd79a1f505406b7d09e7a5bdc6965
Max <mveasey87@gmail.com>
2016-11-16 09:50:16 +0000

    added SearchMealsActivity & layout.xml file

---
+++ app/src/main/java/com/example/user/foodtracker/SearchMealsActivity.java
@@ -0,0 +1,13 @@
+package com.example.user.foodtracker;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by user on 16/11/2016.
+ */
+
+public class SearchMealsActivity extends AppCompatActivity {
+
+
+
+}
